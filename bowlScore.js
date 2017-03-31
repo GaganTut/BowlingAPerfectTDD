522818b31f19a8c40d279de4f3f08d5f769ba79d
GaganTut <gstut@live.com>
2017-03-31 12:53:03 -1000

    Created and passed tests for all strikes/spares

---
+++ bowlScore.js
@@ -0,0 +1,57 @@
+module.exports = function(scoreData) {
+  function checkValid(scoreData) {
+    if(!Array.isArray(scoreData) || scoreData.length !== 10) {
+      throw new Error("Not A Valid Bowling Input");
+    } else {
+      for (var i = 0; i < scoreData.length; i++) {
+        for (var k in scoreData[i]) {
+          if (typeof scoreData[i][k] !== "number") {
+            throw new Error("Not A Valid Bowling Input");
+          } else {
+            return true;
+          }
+        }
+      }
+    }
+  }
+
+  if(checkValid(scoreData)) {
+    let totalScore = 0;
+    let currentFrame = 0;
+    let bonusPoints = 0;
+
+    for (var i = 0; i < scoreData.length; i++) {
+      if (scoreData[i].a === 10) {
+        currentFrame += scoreData[i].a;
+        if (scoreData[i + 1] !== undefined) {
+          bonusPoints += scoreData[i + 1].a;
+          if (scoreData[i + 1].a === 10) {
+            if (scoreData[i + 2] !== undefined) {
+              bonusPoints += scoreData[i + 2].a;
+            } else {
+              bonusPoints += scoreData[i + 1].b;
+            }
+          }
+        } else {
+          bonusPoints += scoreData[i].b;
+          bonusPoints += scoreData[i].c;
+        }
+      } else {
+        currentFrame += scoreData[i].a;
+        currentFrame += scoreData[i].b;
+
+        if (currentFrame === 10) {
+          if(scoreData[i + 1] !== undefined) {
+            bonusPoints += scoreData[i + 1].a;
+          } else {
+            bonusPoints += scoreData[i].c;
+          }
+        }
+      }
+      totalScore += currentFrame;
+      currentFrame = 0;
+    }
+    totalScore += bonusPoints;
+    return totalScore;
+  }
+};
--- test/bowlScore.spec.js
+++ test/bowlScore.spec.js
@@ -46,8 +46,197 @@ let game1 = [
   }
 ];
 
+let game2 = [
+  {
+    a: 0,
+    b: 0
+  },
+  {
+    a: 0,
+    b: 0
+  },
+  {
+    a: 0,
+    b: 0
+  },
+  {
+    a: 0,
+    b: 0
+  },
+  {
+    a: 0,
+    b: 0
+  },
+  {
+    a: 0,
+    b: 0
+  },
+  {
+    a: 0,
+    b: 0
+  },
+  {
+    a: 0,
+    b: 0
+  },
+  {
+    a: 0,
+    b: 0
+  },
+  {
+    a: 0,
+    b: 0,
+  }
+];
+
+let game3 = [
+  {
+    a: 10
+  },
+  {
+    a: 10
+  },
+  {
+    a: 10
+  },
+  {
+    a: 10
+  },
+  {
+    a: 10
+  },
+  {
+    a: 10
+  },
+  {
+    a: 10
+  },
+  {
+    a: 10
+  },
+  {
+    a: 10
+  },
+  {
+    a: 10,
+    b: 10,
+    c: 10
+  }
+];
+
+let game4 = [
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 5,
+    b: 5,
+    c: 5
+  }
+];
+
+let game5 = [
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 10
+  },
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 10
+  },
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 10
+  },
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 10
+  },
+  {
+    a: 5,
+    b: 5
+  },
+  {
+    a: 5,
+    b: 5,
+    c: 5
+  }
+];
+
+
+
 describe("score function", () => {
   it("should be a function", () => {
     expect(score).to.be.a("function");
+  });
+
+  it("should only take in an array of data", () => {
+    expect(score.bind(null, 19)).to.throw(Error);
+    expect(score.bind(null, {a: 12})).to.throw(Error);
+    expect(score.bind(null, NaN)).to.throw(Error);
+    expect(score.bind(null, "Bowling")).to.throw(Error);
+  });
+
+  it("should only take an array of 10 objects", () => {
+    expect(score.bind(null, [4, 5, 9])).to.throw(Error);
+    expect(score.bind(null, [{4: 5}, {a: 7}, {b: 2}])).to.throw(Error);
+  });
+
+  it("should be able to calculate a perfect game", () => {
+    expect(score(game3)).to.equal(300);
+    expect(score(game2)).to.equal(0);
+  });
+
+  it("should calculate spares", () => {
+    expect(score(game4)).to.equal(150);
+  });
+
+  it("should calculate mixed scores", () => {
+    expect(score(game5)).to.equal(190);
   });
 });
